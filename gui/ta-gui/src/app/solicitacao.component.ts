fe24bf9c7fed32d5cdb61b7a0c65879eefb45605
douglas <dfcs@cin.ufpe.br>
2018-12-07 23:55:50 -0300

    FrontEnd da pagina de solicitação feita

--- gui/ta-gui/src/app/solicitacao.component.ts
+++ gui/ta-gui/src/app/solicitacao.component.ts
@@ -2,7 +2,8 @@ import {Component,OnInit} from '@angular/core';
 import {NgModule} from '@angular/core';
 @Component({
     selector:"solicitacao",
-    templateUrl:'./solicitacao.component.html'
+    templateUrl:'./solicitacao.component.html',
+    styleUrls: ['./solicitacao.component.css']
 })
 export class SolicitacaoComponent implements OnInit{
     ngOnInit():void{
